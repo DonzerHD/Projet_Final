4de2f2c3f936c373b75d312ed1c0a7af4700a429
DonzerHD <thomas.lemay59@outlook.fr>
2024-08-16 16:59:32 +0200

    ajout du hallowed host

--- App_web/src/app_web/settings.py
+++ App_web/src/app_web/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2c=g4!6w0hwus!(1827%nu_%dt$*8)hwx7&uy0akc=m7rlkanz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoweb-b0fehsb3dncdbfg9.francecentral-01.azurewebsites.net']
 
 
 # Application definition
