d596a505141e9b4d459d00c2ce4eca6b2d6a2a8c
DonzerHD <thomas.lemay59@outlook.fr>
2024-10-04 23:34:46 +0200

    ajout lien api

--- App_web/src/movies/views.py
+++ App_web/src/movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 import requests
 
-API_BASE_URL = "https://3c5a-2a01-cb0c-1096-a600-3638-47b5-4624-2e02.ngrok-free.app/"  # URL de base de l'API FastAPI
+API_BASE_URL = "https://4c4d-2a01-cb0c-1096-a600-3638-47b5-4624-2e02.ngrok-free.app/"  # URL de base de l'API FastAPI
 
 import jwt
 
