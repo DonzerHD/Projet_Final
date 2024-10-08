3fdc7c100619d83dd39d2ad759116a3aa581386a
Thomas59 <thomas.lemay59@outlook.fr>
2024-10-08 12:38:24 +0200

    ajout de l'api dans les views.py

--- App_web/src/movies/views.py
+++ App_web/src/movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 import requests
 
-API_BASE_URL = "https://885c-2a01-cb0c-1096-a600-7e03-33c1-5bc5-4cc5.ngrok-free.app"  # URL de base de l'API FastAPI
+API_BASE_URL = "https://f8c2-45-81-84-9.ngrok-free.app"  # URL de base de l'API FastAPI
 
 import jwt
 
